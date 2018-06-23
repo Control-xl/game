402418c4a4129f2f7dbbd9328a83b5bb142dbf17
wuzy38 <843404313@qq.com>
2018-06-23 15:53:17 +0800

    weapon

---
+++ weapon.py
@@ -0,0 +1,11 @@
+from settings import Settings
+class Weapon(settings):
+    self.settings = settings
+    self.bullets = []
+    self.sword = {
+        "center" : 0
+        "radius" : 0
+    }
+    self.fist = rect
+    self.weapon = settings.weapon["fist"]
+
